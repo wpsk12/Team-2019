d64b601f0524ee80b90aa90048fde26920577560
JamesAmyouny <46818522+JamesAmyouny@users.noreply.github.com>
2019-01-23 09:13:45 -0500

    Updated
    updated

--- team7.py
+++ team7.py
@@ -6,8 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'JamesAmyouny' # Only 10 chars displayed.
+strategy_name = 'always betray'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
