0dce288856363ff26cdd60cce95f4b2a6ff4f8cf
JonathanD715 <jonathandambrosio511@gmail.com>
2019-01-28 08:30:58 -0500

    Update team5.py

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Jonathan' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Patriots' # Only 10 chars displayed.
+strategy_name = 'Rock Paper'
+strategy_description = 'B if it is found within the last three turns, else C'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +25,10 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if 'b' in their_history[-3]:
+        return 'b'
+    else:
+        return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
