557505de27ff15e67054a7bc57096fda663d29bc
QTran0831 <46818521+QTran0831@users.noreply.github.com>
2019-01-29 08:56:15 -0500

    Update team7.py

--- team7.py
+++ team7.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'CQ' # Only 10 chars displayed.
+strategy_name = 'Logic'
+strategy_description = 'Always betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,14 +26,16 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return 'b'
 
-    
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
     Returns True or False, dpending on whether result was as expected.
     '''
+    
+    return 'b'
+    
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:
         return True
@@ -65,4 +67,6 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')          
+              
+   
